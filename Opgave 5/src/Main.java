ff0d6b5b5c87c3e158750c3ed482b4a9ec082cdd
MojoOno <Ffranck0406@gmail.com>
2024-05-23 11:50:45 +0200

    tilføjet main til git

---
+++ Opgave 5/src/Main.java
@@ -0,0 +1,53 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Garden garden = new Garden();
+
+
+        //Making new plant objects
+        Plant tree1 = new Tree("Oak", LocalDate.of(1964,6,4), 20f);
+        Plant tree2 = new Tree("Sakura", LocalDate.of(1977,6,4), 7f);
+        Plant tree3 = new Tree("Bonzai", LocalDate.of(1977,6,4), 0.3f);
+
+        //Måske laves og addes til en ArrayListe??
+        Plant flower1 = new Flower("My favourite red Rose", LocalDate.of(2023, 7, 20), "Red");
+        Plant flower2 = new Flower("My second favourite red Rose", LocalDate.of(2023, 7, 20), "Red");
+        Plant flower3 = new Flower("Mr. White Rose", LocalDate.of(2023, 7, 20), "White");
+        Plant flower4 = new Flower("I used to be a red Rose", LocalDate.of(2023, 7, 20), "White");
+        Plant flower5 = new Flower("I'm just here for the water Rose", LocalDate.of(2023, 7, 20), "Violet");
+        Plant flower6 = new Flower("Rose Room", LocalDate.of(2023, 7, 20), "Violet");
+
+        FruitTree fruitTree1 = new FruitTree("Apple Tree", LocalDate.of(1945, 3, 15), 5f);
+        Plant fruitTree2 = new FruitTree("Plum Tree", LocalDate.of(1945, 3, 15), 4.5f);
+
+        garden.addPlant(tree1);
+        garden.addPlant(tree2);
+        garden.addPlant(tree3);
+        garden.addPlant(flower1);
+        garden.addPlant(flower2);
+        garden.addPlant(flower3);
+        garden.addPlant(flower4);
+        garden.addPlant(flower5);
+        garden.addPlant(flower6);
+        garden.addPlant(fruitTree1);
+        garden.addPlant(fruitTree2);
+
+        //Registering yield of my fruit trees
+        fruitTree1.registerYield(10f);
+        fruitTree1.registerYield(15f);
+        fruitTree1.registerYield(7f);
+
+        ((FruitTree) fruitTree2).registerYield(4f);
+        ((FruitTree) fruitTree2).registerYield(6f);
+        ((FruitTree) fruitTree2).registerYield(7f);
+
+        garden.getTotalYield();
+        garden.displayFlowersOfColour("Red");
+
+
+
+
+
+    }
+}
