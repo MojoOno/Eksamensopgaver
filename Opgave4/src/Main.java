7b7f2982c2e01e00f6095a0593f9a469f83b2789
MojoOno <Ffranck0406@gmail.com>
2024-05-23 11:51:49 +0200

    no changes

---
+++ Opgave4/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Team team = new Team("Slubberterne", new ArrayList<>());
+        Player juniorPlayer = new JuniorPlayer("Høgen");
+        Player juniorPlayer2 = new JuniorPlayer("Oksen");
+        Player standardPlayer = new StandardPlayer("Ørnen");
+        Player standardPlayer2 = new StandardPlayer("Hunden");
+
+        team.playerList.add(juniorPlayer);
+        team.playerList.add(juniorPlayer2);
+        team.playerList.add(standardPlayer);
+        team.playerList.add(standardPlayer2);
+
+        juniorPlayer.addPoints(10);
+        juniorPlayer.addPoints(15);
+        juniorPlayer.addPoints(7);
+
+        standardPlayer.addPoints(10);
+        standardPlayer.addPoints(15);
+        standardPlayer.addPoints(7);
+
+        team.addPoints(8);
+
+        System.out.println(juniorPlayer);
+        System.out.println(juniorPlayer2);
+        System.out.println(standardPlayer);
+        System.out.println(standardPlayer2);
+        System.out.println(team);
+
+    }
+}
