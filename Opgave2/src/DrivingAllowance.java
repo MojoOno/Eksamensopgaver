b9cc98dc0a3a569708fe436d8cd67301800375da
MojoOno <Ffranck0406@gmail.com>
2024-05-24 12:12:32 +0200

    opgave 2 done

--- Opgave2/src/DrivingAllowance.java
+++ Opgave2/src/DrivingAllowance.java
@@ -1,30 +1,63 @@
 import java.util.Scanner;
 
 public class DrivingAllowance {
+
     String[] months = {"", "Januar", "Februar", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
     int totalCashBack;
+    int moneyBack;
     Scanner scan = new Scanner(System.in);
 
-    public DrivingAllowance(){
+    public DrivingAllowance() {
         totalCashBack = 0;
+    }
+
+    // metode som holder vores inputs
+    public int[] createDrivingAllowanceFromString() {
+        int month = 0;
+        int timesDriven = 0;
+        try {
+            String[] input = scan.nextLine().split(",");                   // Gør at jeg kan indtaste flere ting på samme linje og at det skal separeres ved ","
+            month = Integer.parseInt(input[0]);                                  // Sætter month til at være og en int
+            if (month <= 0 || month > 12) {
+                System.out.println("Invalid choice of month. Try again");
+                return createDrivingAllowanceFromString();
+            }
+            timesDriven = Integer.parseInt(input[1]);                           // lagrer vores input som en int
+            if (timesDriven <= 0 || timesDriven > 10) {
+                System.out.println("Your times driven is either 0 or more than 10. Try again");
+                return createDrivingAllowanceFromString();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Error. Input must be a number. Try again");
+            return createDrivingAllowanceFromString();
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("You only gave one parameter, and thus an error happened. Try again");
+            return createDrivingAllowanceFromString();
+        }
+        return new int[]{month, timesDriven};
+    }
+
+    public int calculateCashBack(int month, int timesDriven) {
+        // Beregn godtgørelse med ternary if
+        int moneyBack = (month <= 6) ? timesDriven * 60 : timesDriven * 80;     // Smid i metode for sig
+        totalCashBack += moneyBack;                                             // Sætter totalCashBack til at være = sig selv + moneyBack
+        return moneyBack;
 
     }
 
-    public void registerDrivesDialog(){
+    public void registerDrivesDialog() {
         System.out.println("Velkommen til RubberDuck BK's kørselsregistrering");
-        while(true) {
+        while (true) {
             System.out.println("Angiv din kørsel nedenfor ved at skrive 1-12 for måned og antal kørsler i givne måned");
             System.out.println("Indtast venligst måned + antal gange (f.eks. 1,3)");
 
-            String[] input = scan.nextLine().split(","); // Gør at jeg kan indtaste flere ting på samme linje og at det skal separeres ved ","
-            int month = Integer.parseInt(input[0]); // sætter month til at være og en int fra 1-12
-            int timesDriven = Integer.parseInt(input[1]); // lagrer vores input som en int
+            int[] input = createDrivingAllowanceFromString();
+            int month = input[0];
+            int timesDriven = input[1];
+            int moneyBack = calculateCashBack(month, timesDriven);
 
-            //Beregn godtgørelse med ternary if
-            int moneyBack = (month <= 6) ? timesDriven * 60 : timesDriven * 80;
-
-            totalCashBack += moneyBack; // Sætter totalCashBack til at være = sig selv + moneyBack
-            System.out.println("Godtgørelse for " + months[month] + ":" + moneyBack + ",-"); //printer hvor meget tilbage i den givne måned.
+            System.out.println("Godtgørelse for " + months[month] + ": " + moneyBack + ",-"); //printer hvor meget tilbage i den givne måned.
 
             System.out.println("Forsæt med at registrere kørsel Y/N");
             String registerMore = scan.nextLine().toUpperCase();
@@ -36,7 +69,6 @@ public class DrivingAllowance {
         scan.close();
 
     }
-
 
 
 }
